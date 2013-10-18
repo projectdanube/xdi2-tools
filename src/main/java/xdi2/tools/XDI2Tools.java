0af2bfa77a7ed2cd0606467bb27a04cc35e675ad
peacekeeper <markus.sabadello@gmail.com>
2013-10-18 13:27:20 -0700

    new command

--- src/main/java/xdi2/tools/XDI2Tools.java
+++ src/main/java/xdi2/tools/XDI2Tools.java
@@ -13,6 +13,7 @@ import xdi2.tools.commands.CommandDiscovery;
 import xdi2.tools.commands.CommandDumpGraph;
 import xdi2.tools.commands.CommandGenerateDigestSecretToken;
 import xdi2.tools.commands.CommandListClassLoaders;
+import xdi2.tools.commands.CommandListMessagingTargetFactorys;
 import xdi2.tools.commands.CommandListMessagingTargets;
 import xdi2.tools.commands.CommandListPlugins;
 import xdi2.tools.commands.CommandMessageGraph;
@@ -24,6 +25,7 @@ public class XDI2Tools {
 	private final static Command[] commands = new Command[] {
 		new CommandListPlugins(),
 		new CommandListClassLoaders(),
+		new CommandListMessagingTargetFactorys(),
 		new CommandListMessagingTargets(),
 		new CommandDumpGraph(),
 		new CommandMessageGraph(),

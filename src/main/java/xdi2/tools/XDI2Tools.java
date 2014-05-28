1c1c4cbacfbaeaec0464176381fc26424ff14df6
peacekeeper <markus.sabadello@gmail.com>
2014-05-28 17:46:21 +0200

    new command rebuild-graphs

--- src/main/java/xdi2/tools/XDI2Tools.java
+++ src/main/java/xdi2/tools/XDI2Tools.java
@@ -20,6 +20,7 @@ import xdi2.tools.commands.CommandMaintenance;
 import xdi2.tools.commands.CommandMessageGraph;
 import xdi2.tools.commands.CommandMessageGraphs;
 import xdi2.tools.commands.CommandMigrateGraphs;
+import xdi2.tools.commands.CommandRebuildGraphs;
 import xdi2.tools.commands.CommandRestoreGraphs;
 import xdi2.transport.exceptions.Xdi2TransportException;
 
@@ -33,6 +34,7 @@ public class XDI2Tools {
 		new CommandDumpGraph(),
 		new CommandBackupGraphs(),
 		new CommandRestoreGraphs(),
+		new CommandRebuildGraphs(),
 		new CommandMessageGraph(),
 		new CommandMessageGraphs(),
 		new CommandMigrateGraphs(),

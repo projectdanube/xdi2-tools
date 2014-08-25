2532c67aa19de493b46860b33a229179809bd665
peacekeeper <markus@projectdanube.org>
2014-08-25 20:08:48 +0200

    URL -> URI

--- src/main/java/xdi2/tools/commands/CommandDiscovery.java
+++ src/main/java/xdi2/tools/commands/CommandDiscovery.java
@@ -1,6 +1,7 @@
 package xdi2.tools.commands;
 
 import java.io.StringWriter;
+import java.net.URI;
 import java.util.Map;
 
 import xdi2.client.http.XDIHttpClient;
@@ -39,9 +40,9 @@ public class CommandDiscovery implements Command {
 
 			// from authority
 
-			if (discoveryResultRegistry != null && discoveryResultRegistry.getXdiEndpointUri() != null) {
+			if (discoveryResultRegistry != null && discoveryResultRegistry.getXdiEndpointUrl() != null) {
 
-				discoveryResultAuthority = discoveryClient.discoverFromAuthority(discoveryResultRegistry.getXdiEndpointUri(), discoveryResultRegistry.getCloudNumber(), null);
+				discoveryResultAuthority = discoveryClient.discoverFromAuthority(discoveryResultRegistry.getXdiEndpointUrl(), discoveryResultRegistry.getCloudNumber(), null);
 			}
 
 			// output result
@@ -54,7 +55,7 @@ public class CommandDiscovery implements Command {
 				writer.write("Information from registry:\n\n");
 
 				writer.write("Cloud Number: " + discoveryResultRegistry.getCloudNumber() + "\n");
-				writer.write("XDI Endpoint URI: " + discoveryResultRegistry.getXdiEndpointUri() + "\n");
+				writer.write("XDI Endpoint URI: " + discoveryResultRegistry.getXdiEndpointUrl() + "\n");
 				writer.write("Signature Public Key: " + discoveryResultRegistry.getSignaturePublicKey() + "\n");
 				writer.write("Encryption Public Key: " + discoveryResultRegistry.getEncryptionPublicKey() + "\n");
 
@@ -63,7 +64,7 @@ public class CommandDiscovery implements Command {
 					writer.write("Services: (none)\n");
 				} else {
 
-					for (Map.Entry<XDIAddress, String> endpointUri : discoveryResultRegistry.getEndpointUris().entrySet()) {
+					for (Map.Entry<XDIAddress, URI> endpointUri : discoveryResultRegistry.getEndpointUris().entrySet()) {
 
 						writer.write("Service " + endpointUri.getKey() + ": " + endpointUri.getValue() + "\n");
 					}
@@ -80,7 +81,7 @@ public class CommandDiscovery implements Command {
 				writer2.write("Information from authority:\n\n");
 
 				writer2.write("Cloud Number: " + discoveryResultAuthority.getCloudNumber() + "\n");
-				writer2.write("XDI Endpoint URI: " + discoveryResultAuthority.getXdiEndpointUri() + "\n");
+				writer2.write("XDI Endpoint URI: " + discoveryResultAuthority.getXdiEndpointUrl() + "\n");
 				writer2.write("Signature Public Key: " + discoveryResultAuthority.getSignaturePublicKey() + "\n");
 				writer2.write("Encryption Public Key: " + discoveryResultAuthority.getEncryptionPublicKey() + "\n");
 
@@ -89,7 +90,7 @@ public class CommandDiscovery implements Command {
 					writer2.write("Services: (none)\n");
 				} else {
 
-					for (Map.Entry<XDIAddress, String> endpointUri : discoveryResultAuthority.getEndpointUris().entrySet()) {
+					for (Map.Entry<XDIAddress, URI> endpointUri : discoveryResultAuthority.getEndpointUris().entrySet()) {
 
 						writer2.write("Service " + endpointUri.getKey() + ": " + endpointUri.getValue() + "\n");
 					}
